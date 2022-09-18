740ceedb389c3edd2dfe6bf09ae89d14360bc805
chelseacampbell22 <110561906+chelseacampbell22@users.noreply.github.com>
2022-09-18 13:53:10 -0500

    Changed units to be consistent

--- weather/weather.js
+++ weather/weather.js
@@ -71,7 +71,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
